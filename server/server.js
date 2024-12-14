ae3ff6bfbec7a6bc6f1facfc48dab2a208f73ed8
ZaoShang12 <142527679+ZaoShang12@users.noreply.github.com>
2024-12-14 16:51:13 -0600

    server and database setup

--- server/server.js
+++ server/server.js
@@ -6,3 +6,24 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 });
+
+const mysql = require('mysql2');
+
+// Create a connection to the database
+const db = mysql.createConnection({
+    host: 'ec2-3-219-93-177.compute-1.amazonaws.com',
+    user: 'video',
+    password: 'video4Ever',
+    database: 'Video4Ever'
+});
+
+// Connect to the database
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MySQL database.');
+});
+
+
